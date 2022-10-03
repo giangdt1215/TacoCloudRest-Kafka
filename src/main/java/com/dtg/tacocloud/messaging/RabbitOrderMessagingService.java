78a338f366b0e39c9652c0c6d149af1a1966b173
Dang Giang <gianggiang1512@gmail.com>
2022-10-03 22:30:13 +0700

    Add send message feature using RabbitMQ(AMQP)

---
+++ src/main/java/com/dtg/tacocloud/messaging/RabbitOrderMessagingService.java
@@ -0,0 +1,37 @@
+package com.dtg.tacocloud.messaging;
+
+import com.dtg.tacocloud.model.TacoOrder;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitOrderMessagingService implements OrderMessagingService{
+
+    private RabbitTemplate rabbit;
+
+    public RabbitOrderMessagingService(RabbitTemplate rabbit){
+        this.rabbit = rabbit;
+    }
+
+    @Override
+    public void sendOrder(TacoOrder order) {
+        //Use send() method of RabbitTemplate
+//        MessageConverter converter = rabbit.getMessageConverter();
+//        MessageProperties props = new MessageProperties();
+//        props.setHeader("X_ORDER_SOURCE", "WEB");
+//        Message message = converter.toMessage(order, props);
+//        rabbit.send("tacocloud.order", message);
+
+        //Use convertAndSend() method of RabbitTemplate, use MessagePostProcessor for setting Header
+        rabbit.convertAndSend("tacocloud.order.queue", order, message -> {
+            MessageProperties props = message.getMessageProperties();
+            props.setHeader("X_ORDER_SOURCE", "WEB");
+            return message;
+        });
+    }
+}
