1428bb1b3cebab5e564c55ce4b2565a007e4007d
GiangDT <giangdt1215@gmail.com>
2022-10-08 18:08:11 +0700

    Remove unnecessary messageConverter bean

--- src/main/java/com/dtg/tacocloud/messaging/MessageConfig.java
+++ src/main/java/com/dtg/tacocloud/messaging/MessageConfig.java
@@ -1,14 +1,8 @@
 package com.dtg.tacocloud.messaging;
 
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MessageConfig {
 
-    @Bean
-    public Jackson2JsonMessageConverter messageConverter(){
-        return new Jackson2JsonMessageConverter();
-    }
 }
